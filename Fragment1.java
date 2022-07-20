2d4e096c85150d5e0173ef1b3d675b86007d15f1
Hyung Won <91198709+HyungWonKang@users.noreply.github.com>
2022-07-21 01:53:09 +0900

    camera fxx
    camera

--- Fragment1.java
+++ Fragment1.java
@@ -1,24 +1,34 @@
 package android.scroll.tlllllll;
 
+import static android.app.Activity.RESULT_OK;
 import static android.scroll.tlllllll.SelectDateFragment.DATE;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,32 +39,68 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.pedro.library.AutoPermissions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.zip.Inflater;
 
 
 public class Fragment1 extends Fragment {
 
-    ImageView imageView;
+    private static final String TAG = "Fragment1";
+
+    Context context;
+    OnTabItemSelectedListener listener;
+    ImageView pictureImageView;
+    int selectPhotoMenu;
+
+    boolean isPhotoCaptured;
+    boolean isPhotoFileSaved;
+    boolean isPhotoCanceled;
+    File file;
+    Bitmap resultPhotoBitmap;
     Uri uri;
+
+
     SQLiteDatabase database;
     String table1 = "tableString";
     String table2 = "tableImage";
-    public Context context;
     ViewGroup rootView;
     DatePickerDialog datePickerDialog;
     FloatingActionButton floatingActionButton;
     public static EditText date;
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+        if(context instanceof OnTabItemSelectedListener) {
+            listener = (OnTabItemSelectedListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        if(context != null) {
+            context = null;
+            listener = null;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         rootView = (ViewGroup) inflater.inflate(R.layout.fragment_1, container, false);
 
         date = (EditText) rootView.findViewById(R.id.date);
+        pictureImageView = rootView.findViewById(R.id.pictureImageView);
         floatingActionButton = (FloatingActionButton) rootView.findViewById(R.id.floatingActionButton);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,11 +110,256 @@ public class Fragment1 extends Fragment {
 
             }
         });
+//        imageView = (ImageView) rootView.findViewById(R.id.imageView); // 이거 무조건 setContentView 뒤에 써야함!!!!!!!!!!!!
+        pictureImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPhotoCaptured || isPhotoFileSaved) {
+                    showDialog(AppConstants.CONTENT_PHOTO_EX);
+                } else {
+                    showDialog(AppConstants.CONTENT_PHOTO);
+                }
+            }
+        });
 
+        //AutoPermissions.Companion.loadAllPermissions(this, 101);
         return rootView;
-//        imageView = findViewById(R.id.imageView); // 이거 무조건 setContentView 뒤에 써야함!!!!!!!!!!!!
     }
 
+    public void showDialog(int id) {
+        AlertDialog.Builder builder = null;
+
+        switch (id) {
+            case AppConstants.CONTENT_PHOTO:
+                builder = new AlertDialog.Builder(context);
+
+                builder.setTitle("사진 메뉴 선택");
+                builder.setSingleChoiceItems(R.array.array_photo, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        selectPhotoMenu = which;
+                    }
+                });
+                builder.setPositiveButton("선택", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(selectPhotoMenu ==0) {
+                            showPhotoCaptureActivity();
+                        } else if(selectPhotoMenu ==1) {
+                            showPhotoSelectionActivity();
+                        }
+                    }
+                });
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                break;
+            case AppConstants.CONTENT_PHOTO_EX:
+                builder = new AlertDialog.Builder(context);
+
+                builder.setTitle("사진 메뉴 선택");
+                builder.setSingleChoiceItems(R.array.array_photo_ex, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        selectPhotoMenu = which;
+                    }
+                });
+                builder.setPositiveButton("선택", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(selectPhotoMenu ==0) {
+                            showPhotoCaptureActivity();
+                        } else if(selectPhotoMenu ==1) {
+                            showPhotoSelectionActivity();
+                        } else if(selectPhotoMenu ==2) {
+                            isPhotoCanceled = true;
+                            isPhotoCaptured = false;
+
+                            pictureImageView.setImageResource(R.drawable.picture1);
+                        }
+                    }
+                });
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                break;
+            default:
+                break;
+
+        }
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void showPhotoCaptureActivity() {
+
+        MainActivity activity = (MainActivity) getActivity();
+        activity.takePicture();
+
+//        try {
+//            file = createFile();
+//            if (file.exists()) {
+//                file.delete();
+//            }
+//
+//            file.createNewFile();
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        if(Build.VERSION.SDK_INT >= 24) {
+//            uri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID, file);
+//        } else {
+//            uri = Uri.fromFile(file);
+//        }
+//
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+//
+//        if(intent.resolveActivity(context.getPackageManager())!= null) {
+//            startActivityForResult(intent, AppConstants.REQ_PHOTO_CAPTURE);
+//        }
+    }
+
+    private File createFile() {
+        String filename = "capture.jpg";
+        File storageDir = Environment.getExternalStorageDirectory();
+        File outFile = new File(storageDir, filename);
+
+        return outFile;
+    }
+
+    private String createFilename() {
+        Date curDate = new Date();
+        String curDateStr = String.valueOf(curDate.getTime());
+
+        return curDateStr;
+    }
+
+    public void setPicture(String picturePath, int sampleSize) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = sampleSize;
+        resultPhotoBitmap = BitmapFactory.decodeFile(picturePath, options);
+
+        pictureImageView.setImageBitmap(resultPhotoBitmap);
+    }
+
+    public void showPhotoSelectionActivity() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, AppConstants.REQ_PHOTO_SELECTION);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (intent != null) {
+            switch (requestCode) {
+                case AppConstants.REQ_PHOTO_CAPTURE:  // 사진 찍는 경우
+                    Log.d(TAG, "onActivityResult() for REQ_PHOTO_CAPTURE.");
+
+                    Log.d(TAG, "resultCode : " + resultCode);
+
+
+
+                    Bundle extras = intent.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("intent");
+                    pictureImageView.setImageBitmap(imageBitmap);
+
+
+//                    setPicture(file.getAbsolutePath(), 8);
+//                    resultPhotoBitmap = decodeSampledBitmapFromResource(file, pictureImageView.getWidth(), pictureImageView.getHeight());
+//                    pictureImageView.setImageBitmap(resultPhotoBitmap);
+
+
+                    break;
+
+                case AppConstants.REQ_PHOTO_SELECTION:  // 사진을 앨범에서 선택하는 경우
+                    Log.d(TAG, "onActivityResult() for REQ_PHOTO_SELECTION.");
+
+                    Uri fileUri = intent.getData();
+
+                    ContentResolver resolver = context.getContentResolver();
+
+                    try {
+                        InputStream instream = resolver.openInputStream(fileUri);
+                        resultPhotoBitmap = BitmapFactory.decodeStream(instream);
+                        pictureImageView.setImageBitmap(resultPhotoBitmap);
+
+                        instream.close();
+
+                        isPhotoCaptured = true;
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+            }
+        }
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(File res, int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(res.getAbsolutePath(),options);
+
+//        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeFile(res.getAbsolutePath(),options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height;
+            final int halfWidth = width;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+
+    public void getGallery() {
+
+    }
+
+    public void saveFile(View v) {
+
+    }
+
+    public void deletePage(View v) {
+
+    }
+
+
+}
 
 //    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
 //            new ActivityResultCallback<ActivityResult>() {
@@ -82,7 +373,7 @@ public class Fragment1 extends Fragment {
 //                            imageView.setImageBitmap(bitmap);
 //                        }
 //                        catch (Exception e) {
-//                            Toast.makeText( getApplicationContext(),"실패", Toast.LENGTH_SHORT).show();
+//                            Toast.makeText( getContext(),"실패", Toast.LENGTH_SHORT).show();
 //                        }
 //                    }
 //                }
@@ -93,10 +384,6 @@ public class Fragment1 extends Fragment {
 //        intent.setType("image/*");
 //        startActivityResult.launch(intent);
 //    }
-
-    public void saveFile(View v) {
-
-    }
 
 //    private void createDatabase(String databaseName) {
 //        database = openOrCreateDatabase(databaseName, MODE_PRIVATE, null);
@@ -119,9 +406,3 @@ public class Fragment1 extends Fragment {
 //
 //        );
 //    }
-
-    public void deletePage(View v) {
-
-    }
-
-}
