776286c6ebd711b5846d1375b624951bf068c89c
HyungWonKang <khw2421@uos.ac.kr>
2022-07-22 20:27:11 +0900

    wpqkf
    wpqkf

---
+++ FloatingTest/app/src/main/java/android/scroll/floatingtest/Justee.java
@@ -0,0 +1,16 @@
+package android.scroll.floatingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Justee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_justee);
+
+
+    }
+}
