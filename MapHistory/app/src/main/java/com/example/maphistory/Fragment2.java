6cc74d7affd327c94f73e040a60c90ca66c20c4a
HyungWonKang <khw2421@uos.ac.kr>
2022-09-04 17:26:13 +0900

    fragment back stack
    oo

--- MapHistory/app/src/main/java/com/example/maphistory/Fragment2.java
+++ MapHistory/app/src/main/java/com/example/maphistory/Fragment2.java
@@ -96,12 +96,12 @@ public class Fragment2 extends Fragment {
                 Toast.makeText(getContext(), "선택: " +item.getContents(), Toast.LENGTH_SHORT).show();
                 fragmentNew = new Fragment1();
                 fragmentNew.setItem(item);
-
-                NewAndListActivity ad= (NewAndListActivity) getActivity();
-                ad.bottomNavigation.setSelectedItemId(R.id.tab1);
+//
+//                NewAndListActivity ad= (NewAndListActivity) getActivity();
+//                ad.bottomNavigation.setSelectedItemId(R.id.tab1);
 
                 getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, fragmentNew).commit();
+                        .replace(R.id.container, fragmentNew).addToBackStack(null).commit();
 
 
             }
